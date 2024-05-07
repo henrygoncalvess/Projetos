681c70614967a496448e1f4e910e014e86d6e14f
henrygoncalvess <octanebt@gmail.com>
2024-05-07 20:39:11 -0300

    igual e funções

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,1 +1,6 @@
-var tela = document.getElementById('div')
+var tela = document.getElementById('tela')
+var memoria = []
+var show = document.getElementsByTagName('td')[14]
+function clicou(num=0){
+    tela.innerHTML += num
+}
