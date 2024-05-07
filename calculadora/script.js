3fb25c7049ab3ab8c6e8017293e2cfcfb935578f
henrygoncalvess <octanebt@gmail.com>
2024-05-07 20:49:05 -0300

    numeros e operadores aparecendo

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,6 +1,9 @@
 var tela = document.getElementById('tela')
 var memoria = []
 var show = document.getElementsByTagName('td')[14]
-function clicou(num=0){
+function clicou(num){
     tela.innerHTML += num
+    if (num == 11){
+        tela.innerHTML = ''
+    }
 }
