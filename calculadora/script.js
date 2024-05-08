d75d68839385d997f04a5604912934b4d2fde0c8
henrygoncalvess <octanebt@gmail.com>
2024-05-07 21:04:55 -0300

    só add números

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,9 +1,19 @@
 var tela = document.getElementById('tela')
 var memoria = []
-var show = document.getElementsByTagName('td')[14]
 function clicou(num){
     tela.innerHTML += num
     if (num == 11){
         tela.innerHTML = ''
     }
+    if (num == 111){
+        tela.innerHTML = memoria
+    }else if (num != 11 & num != '+' & num != '-' & num != 'x'){
+        memoria.push(num)
+    }
+}
+function result(){
+    if (num == 11){
+        tela.innerHTML = ''
+    }
+    tela.innerHTML = memoria
 }
