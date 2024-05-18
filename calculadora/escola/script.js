4d933ad6b955a8041bf1c532aaf35eaee1cef2c3
henrygoncalvess <octanebt@gmail.com>
2024-05-18 06:06:31 -0300

    calc terminada

--- calculadora/escola/script.js
+++ calculadora/escola/script.js
@@ -1,57 +1,19 @@
-var lista = []
-var lista2 = []
-var lista3 = []
 var tela = document.getElementById('tela')
 var igual = document.getElementById('igual')
 function cliquei(num=''){
+    igual.innerHTML = '<br>'
     tela.innerText += num
-        if (num == 11){
+    if (num == 11){
         tela.innerText = ''
-        lista = []
-        igual.innerHTML = '<br>'
-        }
-}
-function calcular(conta){
-    var res = 0
-    var res2 = lista2[0] * 2
-    var res3 = 1
-    switch (conta){
-        case '+':
-            lista.push(tela.innerText)
-            tela.innerText = ''
-            break
-        case '-':
-            lista2.push(tela.innerText)
-            tela.innerText = ''
-            break
-        case 'x':
-            lista3.push(tela.innerText)
-            tela.innerText = ''
-            break
-        case '=':
-            lista.push(tela.innerText)
-            lista2.push(tela.innerText)
-            lista3.push(tela.innerText)
-            for (let pos in lista){
-                res += Number(lista[pos])
-            }
-            for (let pos in lista2){
-                res2 -= Number(lista2[pos])
-            }
-            for (let pos in lista3){
-                res3 *= Number(lista3[pos])
-            }
-            if (lista.length > 1){
-                igual.innerText = res
-            }else if (lista2.length > 1){
-                igual.innerText = res2
-            }else if (lista3.length > 1){
-                igual.innerText = res3
-            }
-            tela.innerText = ''
-            lista = []
-            lista2 = []
-            lista3 = []
-            break
     }
+    if (num == '%'){
+        tela.innerText = tela.innerText.replace('%', ' ')
+        tela.innerText += '/100'
+    }
+}
+
+function calcular(){
+    var resultado = eval(tela.innerText)
+    igual.innerText = resultado
+    tela.innerText = ''
 }
