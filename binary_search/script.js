0feca7c36960cc7f30d40736773c69bcb34023a0
henrygoncalvess <octanebt@gmail.com>
2024-06-07 16:58:00 -0300

    repassando do replit

--- binary_search/script.js
+++ binary_search/script.js
@@ -1,3 +1,5 @@
+// import * as conteudo from './conteudo.js';
+
 var titulo = document.getElementById('titulo')
 var parag = document.getElementById('parag')
 var tam2 = document.getElementById('tam2')
@@ -6,20 +8,30 @@ var avan = document.getElementById('avan')
 var tela = document.getElementById('ind')
 var pos = 0
 
-function trocar() {
+function pag1(){
+    setTimeout(() => {
+        ant.style.opacity = 0
+        avan.style.opacity = 100
+        titulo.innerHTML = `O que é "<strong>Binary Search</strong>" <br> ou <br> "<strong>Pesquisa Binária</strong>"?`
+        parag.innerHTML = 'Para entendermos melhor o que é e como funciona uma <strong>Pesquisa Binária</strong>, vamos começar analizando um algoritmo de <mark>Busca Sequencial</mark>'
+    }, 900);
+}
+
+// animação de sumir e aparecer
+function animar(){
     titulo.style.translate = '-400px'
     parag.style.translate = '-400px'
-
     titulo.style.opacity = 0
     parag.style.opacity = 0
+    ant.style.opacity = 0
     avan.style.opacity = 0
-
+    
     setTimeout(() => {
         titulo.style.translate = '400px'
         parag.style.translate = '400px'
         tam2.style.transition = '0ms'
     }, 600);
-
+    
     setTimeout(() => {
         tam2.style.padding = '20px'
         titulo.style.translate = '0px'
@@ -27,49 +39,58 @@ function trocar() {
 
         titulo.style.opacity = 100
         parag.style.opacity = 100
-    }, 1100);
 
-    setTimeout(() => {
         ant.style.opacity = 100
         avan.style.opacity = 100
         tam2.style.transition = '800ms'
-    }, 1200);
+    }, 900);
+}
 
+// quando clicar em AVANÇAR
+function trocar() {
+    animar()
+
+    // se for o começo, página 2
     if (pos == 0) {
         pos += 2
-    } else { pos += 1 }
-    switch (pos) {
-        case 2:
-            tam2.style.padding = '212px 0px'
+    }
+    else { pos += 1 }
 
+    // se a posição for...
+    switch (pos) {
+
+        // página 2
+        case 2:
             setTimeout(() => {
                 titulo.innerHTML = `<strong>Busca Sequencial</strong>`
                 parag.innerHTML = `
         <p>
-            Uma <strong>Busca Sequencial</strong> é o algoritmo mais simples de busca. <br> <br> Ele percorre todos os valores dentro de uma lista comparando as posições com os valores dos elementos.
-            <br>
-            Se a <mark>chave</mark> for igual a algum dos valores, a busca retorna a posição correspondente na lista (Retorna -1 se não encontrar)
-            <br>
-            <br>
-            <mark>Chave:</mark> <strong>7</strong>
-            <br>
-            <br>
-            <pre>
+        Uma <strong>Busca Sequencial</strong> é o algoritmo mais simples de busca. <br> <br> Ele percorre todos os valores dentro de uma lista comparando as posições com os valores dos elementos.
+        <br>
+        Se a <mark>chave</mark> for igual a algum dos valores, a busca retorna a posição correspondente na lista (Retorna -1 se não encontrar)
+        <br>
+        <br>
+        <mark>Chave:</mark> <strong>7</strong>
+        <br>
+        <br>
+        <pre>
 <span class="cod">lista = [<span id="a1" class="a">2</span>,<span id="a2" class="a">5</span>,<span id="a3" class="a">9</span>,<span id="a4" class="a">1</span>,<span id="a5" class="a">0</span>,<span id="b" class="a">7</span>,3 ]</span>
-            </pre>
-            <br>
-            <span class="tela"><strong><span id="d1">&bull;</span><span id="d2">&bull;</span><span
-                        id="d3">&bull;</span><span id="d4">&bull;</span></strong><span id="pos">Valor encontrado
-                    <br>
-                    Posição: <a>5</a></span>
+        </pre>
+        <br>
+        <span class="tela"><strong><span id="d1">&bull;</span><span id="d2">&bull;</span><span
+                    id="d3">&bull;</span><span id="d4">&bull;</span></strong><span id="pos">Valor encontrado
+                <br>
+                Posição: <a>5</a></span>
         </p>`
-            }, 1100);
+            }, 900);
             break
+
+        // página 3
         case 3:
             setTimeout(() => {
+                // some com o botão de avançar
                 avan.style.opacity = 0
-            }, 1200);
-            setTimeout(() => {
+                
                 titulo.innerHTML = `<strong>Desvantagem</strong>`
                 parag.innerHTML = `
         <p>
@@ -82,14 +103,17 @@ function trocar() {
             
         </p>
                 `
-            }, 1100)
+            }, 1000)
             break
+
+        // final
         case 4:
+            // não deixa o conteúdo desaparecer e sair do lugar
             titulo.style.translate = '0px'
             parag.style.translate = '0px'
-
             titulo.style.opacity = 100
             parag.style.opacity = 100
+            ant.style.opacity = 100
 
             setTimeout(() => {
                 titulo.style.translate = '0px'
@@ -98,40 +122,35 @@ function trocar() {
             }, 600);
 
             setTimeout(() => {
+                titulo.style.translate = '0px'
+                parag.style.translate = '0px'
+                tam2.style.transition = '0ms'
                 avan.style.opacity = 0
-            }, 1200);
+            }, 900);
             pos -= 1
             break
     }
 }
 
+// quando clicar em VOLTAR
 function voltar() {
-    ant.style.opacity = 100
-    titulo.style.translate = '-400px'
-    parag.style.translate = '-400px'
+    animar()
 
-    titulo.style.opacity = 0
-    parag.style.opacity = 0
-
-    setTimeout(() => {
-        titulo.style.translate = '400px'
-        parag.style.translate = '400px'
-    }, 600);
-
-    setTimeout(() => {
-        titulo.style.translate = '0px'
-        parag.style.translate = '0px'
-
-        titulo.style.opacity = 100
-        parag.style.opacity = 100
-    }, 900);
-
-    if (pos == 3 || pos == 2 || pos == 1) {
+    // se a posição não for o início, volta uma página
+    if (pos > 0) {
         pos -= 1
-    } else { pos -= 0 }
+    }
+    else { pos -= 0 }
+
+    // se a posição for...
     switch (pos) {
+
+        // se for o início
+        // não deixa o conteúdo desaparecer e sair do lugar
         case 0:
-            ant.style.opacity = 0
+            titulo.style.opacity = 100
+            parag.style.opacity = 100
+            avan.style.opacity = 100
             titulo.style.translate = '0px'
             parag.style.translate = '0px'
 
@@ -140,21 +159,26 @@ function voltar() {
                 parag.style.translate = '0px'
             }, 600);
 
-            titulo.style.opacity = 100
-            parag.style.opacity = 100
+            setTimeout(() => {
+                ant.style.opacity = 0
+            }, 900);
 
             titulo.innerHTML = `O que é "<strong>Binary Search</strong>" <br> ou <br> "<strong>Pesquisa Binária</strong>"?`
             parag.innerHTML = 'Para entendermos melhor o que é e como funciona uma <strong>Pesquisa Binária</strong>, vamos começar analizando um algoritmo de <mark>Busca Sequencial</mark>'
             break
+
+        // página 1
         case 1:
-            ant.style.opacity = 0
-            setTimeout(() => {
-                titulo.innerHTML = `O que é "<strong>Binary Search</strong>" <br> ou <br> "<strong>Pesquisa Binária</strong>"?`
-                parag.innerHTML = 'Para entendermos melhor o que é e como funciona uma <strong>Pesquisa Binária</strong>, vamos começar analizando um algoritmo de <mark>Busca Sequencial</mark>'
-            }, 900);
+            // some com o botão de voltar
+            avan.style.opacity = 100
+            pag1()
             break
+
+        // página 2
         case 2:
+            // conteúdo
             setTimeout(() => {
+                avan.style.opacity = 100
                 titulo.innerHTML = `<strong>Busca Sequencial</strong>`
                 parag.innerHTML = `
         <p>
