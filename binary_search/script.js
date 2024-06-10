9a18374e9a1d85399acd3ca8904f6d30c7ce68e7
henrygoncalvess <octanebt@gmail.com>
2024-06-10 17:21:20 -0300

    repassando do replit

--- binary_search/script.js
+++ binary_search/script.js
@@ -4,17 +4,51 @@ var tam2 = document.getElementById('tam2')
 var ant = document.getElementById('vol')
 var avan = document.getElementById('avan')
 var tela = document.getElementById('ind')
+var resp1 = document.getElementById('resposta1')
+var resp2 = document.getElementById('resposta2')
+var resp3 = document.getElementById('resposta3')
 var pos = 0
+if (pos == 0){
+    avan.style.display = 'none'
+    ant.style.display = 'none'
+}
+
+function verificar(resposta){
+    animar()
+    
+    switch (resposta) {
+        case 1:
+            pos = 10
+            setTimeout(() => {
+                ant.style.opacity = 100
+                avan.style.display = 'none'
+                titulo.innerHTML = `Em andamento`
+                parag.innerHTML = ''
+            }, 900);
+            break
+            
+        case 2:
+            pos = 1
+            pagina1()
+            break
+        
+        case 3:
+            pos = 10
+            setTimeout(() => {
+                ant.style.opacity = 100
+                avan.style.display = 'none'
+                titulo.innerHTML = `Em andamento`
+                parag.innerHTML = ''
+            }, 900);
+            break
+    }
+}
 
 // quando clicar em AVANÇAR
 function trocar() {
     animar()
 
-    // se for o começo, página 2
-    if (pos == 0) {
-        pos += 2
-    }
-    else { pos += 1 }
+    pos += 1
 
     // posição == página
     switch (pos) {
@@ -71,36 +105,21 @@ function voltar() {
     animar()
 
     // se a posição não for o início, volta uma página
-    if (pos > 0) {
+    if (pos >= 0) {
         pos -= 1
     }
     else { pos -= 0 }
 
     // posição == página
     switch (pos) {
-
+            
         // se for o início
         // não deixa o conteúdo desaparecer e sair do lugar
         case 0:
-            titulo.style.opacity = 100
-            parag.style.opacity = 100
-            avan.style.opacity = 100
-            titulo.style.translate = '0px'
-            parag.style.translate = '0px'
-
-            setTimeout(() => {
-                titulo.style.translate = '0px'
-                parag.style.translate = '0px'
-            }, 600);
-
-            setTimeout(() => {
-                ant.style.opacity = 0
-            }, 900);
+            inicio()
             break
 
         case 1:
-            // some com o botão de voltar
-            avan.style.opacity = 100
             pagina1()
             break
 
@@ -119,9 +138,14 @@ function voltar() {
         case 5:
             pagina5()
             break
-        
+
         case 6:
             pagina6()
+            break
+            
+        case 9:
+            inicio()
+            pos = 0
             break
     }
 }
@@ -132,22 +156,36 @@ function voltar() {
 function animar(){
 
     // move o conteúdo pra esquerda <- e soma com os botões
+    resp1.style.translate = '-100px'
+    resp2.style.translate = '-100px'
+    resp3.style.translate = '-100px'
     titulo.style.translate = '-100px'
     parag.style.translate = '-100px'
+    resp1.style.opacity = 0
+    resp2.style.opacity = 0
+    resp3.style.opacity = 0
     titulo.style.opacity = 0
     parag.style.opacity = 0
     ant.style.opacity = 0
     avan.style.opacity = 0
-    tam2.style.rotateX = '10deg'
-    
+
     // move o conteúdo pra direita -> pra ele aparecer
     setTimeout(() => {
+        resp1.style.translate = '85px'
+        resp2.style.translate = '85px'
+        resp3.style.translate = '85px'
         titulo.style.translate = '85px'
         parag.style.translate = '85px'
     }, 600);
 
     // aparece com o conteúdo e os botões
     setTimeout(() => {
+        resp1.style.display = 'none'
+        resp2.style.display = 'none'
+        resp3.style.display = 'none'
+        resp1.style.translate = '0px'
+        resp2.style.translate = '0px'
+        resp3.style.translate = '0px'
         titulo.style.translate = '0px'
         parag.style.translate = '0px'
 
@@ -156,14 +194,43 @@ function animar(){
 
         ant.style.opacity = 100
         avan.style.opacity = 100
+        ant.style.display = 'block'
+        avan.style.display = 'block'
     }, 900);
 }
 
 // PÁGINAS
 
+function inicio(){
+    setTimeout(() => {
+        titulo.innerHTML = `<h1 id="titulo"></h1>`
+        parag.innerHTML = `<p id="parag">
+            Olá Programador! Hoje você vai aprender o que é uma <strong>"Binary Search"</strong> e como ela funciona.
+            <br>
+            <br>
+            mas antes de começarmos...
+            <br>
+            <br>
+            Você sabe o que é um <strong>"Algoritmo"?</strong>
+            <br>
+            <br>
+            É importante que você selecione qual é o seu nível de conhecimento abaixo, porque você vai conseguir aprender mesmo que não entenda de programação.
+            <br>
+            <br>
+        </p>`
+        ant.style.display = 'none'
+        avan.style.display = 'none'
+        resp1.style.display = 'block'
+        resp2.style.display = 'block'
+        resp3.style.display = 'block'
+        resp1.style.opacity = 100
+        resp2.style.opacity = 100
+        resp3.style.opacity = 100
+    }, 900);
+}
+
 function pagina1(){
     setTimeout(() => {
-        ant.style.opacity = 0
         avan.style.opacity = 100
         titulo.innerHTML = `O que é <strong>Binary Search</strong> <br> ou <br> <strong>Pesquisa Binária?</strong>`
         parag.innerHTML = 'Para entendermos melhor o que é e como funciona uma <strong>Pesquisa Binária</strong>, vamos começar analizando um algoritmo de <mark>Busca Sequencial</mark>'
@@ -303,17 +370,19 @@ function pagina7(){
         // some com o botão de avançar
         avan.style.opacity = 0
 
-        // 10 11 12 15 18 19 21 22 23 25
-        
+        // 10 13 15 18 19 21 23 27
+        //  0  1  2  3  4  5  6  7
+
         titulo.innerHTML = `<strong>Exemplo</strong>`
         parag.innerHTML = `
         <p>
+            <p id="chave">
+                <mark>Chave:</mark> <strong>21</strong>
+            </p>
+            <br>
+            <br>
             Agora o nosso vetor <mark>ORDENADO</mark> tem <span id="cor" class="pag7">8 posições</span>, com os valores :
             <br>
-            <br>
-        <p id="chave">
-            <mark>Chave:</mark> <strong>21</strong>
-        </p>
             <br>
         <p class="posicao">
             <span id="numpag7" class="pag7">11</span>
