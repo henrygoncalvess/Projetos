c7dbbf24d642f2dabe85a7393cf02c33a3f915da
henrygoncalvess <octanebt@gmail.com>
2024-06-21 15:34:14 -0300

    mensagem de escolha

--- jogo_velha/velha.py
+++ jogo_velha/velha.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 lista = [[1,2,3], [4,5,6], [7,8,9]]
 
@@ -6,11 +7,19 @@ jog = 0
 pc = 0
 
 def velha(coluna=0, linha=0):
-      for l in range(0, len(lista)):
-         print(f'{l + 1} ->', end='')
-         for c in range(0, len(lista)):
-            print(' ' * 2 + f'[ {lista[l][c]} ]', end='  ')
-         print('\n')
+      match coluna, linha:
+         case 0, 0:
+            for l in range(0, len(lista)):
+               print(f'{l + 1} ->', end='')
+               for c in range(0, len(lista)):
+                  print(' ' * 2 + f'[ {lista[l][c]} ]', end='  ')
+               print('\n')
+         case _:
+            for l in range(0, len(lista)):
+               for c in range(0, len(lista)):
+                  print(' ' * 2 + f'[ {lista[l][c]} ]', end='  ')
+               print('\n')
+
 
 
 def jogoX():
@@ -31,8 +40,11 @@ def jogoX():
    
    while lin != lista[col][0] and lin != lista[col][1] and lin != lista[col][2]:
       lin = (int(input(f'Escolha sua POSIÇÃO\n[ {lista[col][0]} ]  [ {lista[col][1]} ]  [ {lista[col][2]} ]: ')[0]))
+   
+   print('Você escolheu...\n')
+   sleep(0.800)
 
-
+   velha(1)
       
    print(f"\n{'vitórias':^30}")
    print(f"{f'Você: {jog}    Computador: {pc}':^30}")
