ed56582472cc9fd3d90b0a334bfa44529b6e6cac
henrygoncalvess <octanebt@gmail.com>
2024-06-21 14:19:17 -0300

    jogo da velha

---
+++ jogo_velha/velha.py
@@ -0,0 +1,30 @@
+from random import randint
+
+lista = [[1,2,3], [4,5,6], [7,8,9]]
+
+jog = 0
+pc = 0
+
+def velha():
+      for l in range(0, len(lista)):
+         print(l + 1, end='')
+         for c in range(0, len(lista)):
+            print(' ' * 2 + f'[ {lista[l][c]} ]', end='  ')
+         print('\n')
+
+
+def jogoX():
+   print(f"{'JOGO DA VELHA':^30}\n")
+   
+   velha()
+   
+   #marca = str(input('Escolha sua marca [X] ou [O]: '))
+   
+   col = int(input('Escolha a coluna: '))
+   lin = int(input(f'Escolha uma posição\n[ {lista[col][0]} ]  [ {lista[col][1]} ]  [ {lista[col][2]} ]: '))
+      
+   print(f"\n{'vitórias':^30}")
+   print(f"{f'Você: {jog}    Computador: {pc}':^30}")
+   
+   
+jogoX()
