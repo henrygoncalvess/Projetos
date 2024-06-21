82847b8fededa1fc65f2b09d032310e8c0100758
henrygoncalvess <octanebt@gmail.com>
2024-06-20 21:18:47 -0300

    controlando o giro

--- roleta/script.js
+++ roleta/script.js
@@ -2,10 +2,15 @@ var roda = document.getElementById('roleta')
 var velocidade = 0
 
 function rodar(){
+    roda.style.rotate = '0deg'
     var id;
     velocidade += 20
     roda.style.rotate = `${velocidade}deg`
 
     id = requestAnimationFrame(rodar)
 
+    if (velocidade > 2000){
+        cancelAnimationFrame(id)
+        velocidade = 0
+    }
 }
