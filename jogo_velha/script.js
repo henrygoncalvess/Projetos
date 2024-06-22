b779007d6b87a4685e5dc65eba29524e713fddc1
henrygoncalvess <octanebt@gmail.com>
2024-06-22 17:31:58 -0300

    script das posições

---
+++ jogo_velha/script.js
@@ -0,0 +1,33 @@
+var cima1 = document.getElementById('a1')
+
+function marcar(posicao){
+    switch (posicao){
+        case 1:
+            cima1.style.background = 'black'
+            break
+
+        case 2:
+            break
+        
+        case 3:
+            break
+
+        case 4:
+            break
+
+        case 5:
+            break
+
+        case 6:
+            break
+        
+        case 7:
+            break
+
+        case 8:
+            break
+
+        case 9:
+            break
+    }
+}
