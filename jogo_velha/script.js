4453385224c770f2fb383fb6b18c9c28df86e509
henrygoncalvess <octanebt@gmail.com>
2024-06-22 17:50:23 -0300

    escolha de marca e posições certas

--- jogo_velha/script.js
+++ jogo_velha/script.js
@@ -1,4 +1,14 @@
 var cima1 = document.getElementById('a1')
+var cima2 = document.getElementById('a2')
+var cima3 = document.getElementById('a3')
+
+var meio1 = document.getElementById('b1')
+var meio2 = document.getElementById('b2')
+var meio3 = document.getElementById('b3')
+
+var baixo1 = document.getElementById('c1')
+var baixo2 = document.getElementById('c2')
+var baixo3 = document.getElementById('c3')
 
 function marcar(posicao){
     switch (posicao){
@@ -7,27 +17,35 @@ function marcar(posicao){
             break
 
         case 2:
+            cima2.style.background = 'black'
             break
         
         case 3:
+            cima3.style.background = 'black'
             break
 
         case 4:
+            meio1.style.background = 'black'
             break
 
         case 5:
+            meio2.style.background = 'black'
             break
 
         case 6:
+            meio3.style.background = 'black'
             break
         
         case 7:
+            baixo1.style.background = 'black'
             break
 
         case 8:
+            baixo2.style.background = 'black'
             break
 
         case 9:
+            baixo3.style.background = 'black'
             break
     }
 }
