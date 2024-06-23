1b8549b4c0859fea004e6a44677fcfca52202948
henrygoncalvess <octanebt@gmail.com>
2024-06-23 02:04:53 -0300

    x aparecendo

--- jogo_velha/script.js
+++ jogo_velha/script.js
@@ -1,54 +1,54 @@
 var x = document.getElementsByTagName('label')[0]
 var o = document.getElementsByTagName('label')[1]
 
-var cima1 = document.getElementById('a1')
-var cima2 = document.getElementById('a2')
-var cima3 = document.getElementById('a3')
+var cima1 = document.getElementsByTagName('img')[0]
+var cima2 = document.getElementsByTagName('img')[1]
+var cima3 = document.getElementsByTagName('img')[2]
 
-var meio1 = document.getElementById('b1')
-var meio2 = document.getElementById('b2')
-var meio3 = document.getElementById('b3')
+var meio1 = document.getElementsByTagName('img')[3]
+var meio2 = document.getElementsByTagName('img')[4]
+var meio3 = document.getElementsByTagName('img')[5]
 
-var baixo1 = document.getElementById('c1')
-var baixo2 = document.getElementById('c2')
-var baixo3 = document.getElementById('c3')
+var baixo1 = document.getElementsByTagName('img')[6]
+var baixo2 = document.getElementsByTagName('img')[7]
+var baixo3 = document.getElementsByTagName('img')[8]
 
 function marcar(posicao){
     switch (posicao){
         case 1:
-            cima1.style.background = 'black'
+            cima1.style.opacity = '100'
             break
 
         case 2:
-            cima2.style.background = 'black'
+            cima2.style.opacity = '100'
             break
         
         case 3:
-            cima3.style.background = 'black'
+            cima3.style.opacity = '100'
             break
 
         case 4:
-            meio1.style.background = 'black'
+            meio1.style.opacity = '100'
             break
 
         case 5:
-            meio2.style.background = 'black'
+            meio2.style.opacity = '100'
             break
 
         case 6:
-            meio3.style.background = 'black'
+            meio3.style.opacity = '100'
             break
         
         case 7:
-            baixo1.style.background = 'black'
+            baixo1.style.opacity = '100'
             break
 
         case 8:
-            baixo2.style.background = 'black'
+            baixo2.style.opacity = '100'
             break
 
         case 9:
-            baixo3.style.background = 'black'
+            baixo3.style.opacity = '100'
             break
     }
 }
