6d81581ebe20737cc83325b9fa90aa218c28bd97
henrygoncalvess <octanebt@gmail.com>
2024-06-29 20:21:29 -0300

    roleta gira bem

--- roleta/script.js
+++ roleta/script.js
@@ -1,25 +1,29 @@
 var roda = document.getElementById('roleta')
 var item = document.getElementById('out')
 var velocidade = 0
-var num = 0
+var num = parseInt(Math.random() * 71)
+var mult = num
+
 
 function rodar(){
-    num = parseInt(Math.random() * 61)
-
-    while (num < 45){
-        num = parseInt(Math.random() * 61)
+    while (num < 60){
+        num = parseInt(Math.random() * 71)
+        mult = num
     }
 
-    roda.style.rotate = '0deg'
+    roda.style.rotate = '270deg'
     var id;
-    velocidade += 20
+    velocidade += mult
     roda.style.rotate = `${velocidade}deg`
 
     id = requestAnimationFrame(rodar)
 
-    if (velocidade > num * 60){
+    if (mult <= 0){
         cancelAnimationFrame(id)
         velocidade = 0
+        num = parseInt(Math.random() * 71)
+    }else{
+        mult -= 0.2
     }
 }
 
